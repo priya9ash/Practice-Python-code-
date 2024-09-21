78383d582c313b33d06cc3ca18e2b603240b64a7
deb-ashwin <165624038+deb-ashwin@users.noreply.github.com>
2024-09-21 15:48:31 +0530

    save the changes

--- file1.py
+++ file1.py
@@ -1,18 +1,19 @@
-import re
+# import re 
+# str1="In a land far, far away, where the sun shone brighter and the stars twinkled more brilliantly, there lived a young adventurer named Aiden. Aiden had always been fascinated by the stories of ancient ruins, hidden treasures, and mysterious creatures that lurked in the depths of the unknown. With a heart full of courage and a thirst for exploration, he set out on a journey that would change his life forever.678"
 
-text = "Closing Thoughts: The film concludes at 00:20:00.000, reminding viewers of the importance of 00:20:00.000 preserving these natural wonders for future '\\n\\n1\\n00:05.080 --> 00:09.310\\n' generations."
-
-
-pattern2search = r"\\n\\n\d\\n\d\d:\d\d.\d\d\d --> \d\d:\d\d.\d\d\d\\n"
-
-
-reg = re.compile(pattern2search)
-
-
-match = reg.search(text)  
+# str2=str1.split()
+# list2=[]
+# for i in range(len(str2)):
+#     if len(str2[i])==4 and not str2[i][-1]=="," and not str2[i][-1]==".":
+#       list2.append(str2[i])
 
 
-if match:
-    print(match.group())  
-else:
-    print("No match found.")
+
+# print(list2)
+# cleantext=" ".join(list2)
+# print(cleantext)
+
+fruits = ['orange', 'grape', 'kiwi', 'mango']
+cleantext=" ".join(fruits)
+print(cleantext)
+
